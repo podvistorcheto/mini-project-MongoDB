c3a5c4da55c0b54dc95a32dadd09162a57ef6e98
podvistorcheto <denev.s.deyan@gmail.com>
2020-04-18 07:28:15 +0000

    html first input

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect,request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello World...again"
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+@app.route("/add_task")
+def add_task():
+    return render_template('addtask.html')
 
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-    port=int(os.environ.get("PORT")),
-    debug=True)
+"""if __name__ == "__main__":app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)"""
+
+if __name__ == '__main__':
+    app.run(host=os.getenv("IP", "0.0.0.0"), 
+    port=int(os.getenv("PORT", 8080)),debug=
+    False)
