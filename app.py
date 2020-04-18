86231ac3a7ffca5802f9205d5a2a6bdbd0c51ee3
podvistorcheto <denev.s.deyan@gmail.com>
2020-04-18 14:07:28 +0000

    add task functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect,request, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
@@ -10,22 +10,30 @@ if path.exists("env.py"):
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
-@app.route('/get_tasks')
+@app.route("/get_tasks")
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
-@app.route("/add_task")
+
+@app.route('/add_task')
 def add_task():
-    return render_template('addtask.html')
+    return render_template('addtask.html', categories=mongo.db.categories.find())
+
+
+@app.route("/insert_task", methods=["POST"])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for("get_tasks"))
+
 
 """if __name__ == "__main__":app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)"""
 
 if __name__ == '__main__':
-    app.run(host=os.getenv("IP", "0.0.0.0"), 
-    port=int(os.getenv("PORT", 8080)),debug=
-    False)
+    app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")),debug=False)
